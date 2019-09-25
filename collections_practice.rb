fe806850af3d364f00f11998e418921c49e31664
Anne Fausto <a.r.fausto@gmail.com>
2019-09-25 02:35:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b 
+  end
+end
 
+def sort_array_desc(array)
+  array.sort.reverse do |a,b|
+    a <=> b
+  end
+end 
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = '$'
+  end 
+end   
+
+def find_a(array)
+  
